1115f7458e918025e82d6d59570daaf100b9ab6b
landa000 <landaup000@gmail.com>
2020-11-10 18:04:02 +0900

    201110-2

--- c_code/e_jquery/js/src/1104test_02.js
+++ c_code/e_jquery/js/src/1104test_02.js
@@ -3,6 +3,27 @@
 (function($){
   var miniPro = $('miniProduct');
   var indi = miniPro.children('.indicator');
-  var indi
+  var btn = indi.children('button');
+
+  var product = miniPro.children('.product');
+  var proUl = product.children('ul')
+  var proLi = proUl.children('li')
+
+  var cloneProLi = proLi.eq(-1).clone(true);
+  proUl.prepend(cloneProLi);
+  var re_proLi = proUl.children('li');
+
+  var permission = true;
+  var timed = 300;
+
+  slideN = 0;
+
+  btn.on('click',function(e){
+    e.preventDefault();
+    if(permission === true){
+      
+
+    }//if(permission) End
+  });
 
 })(jQuery);
